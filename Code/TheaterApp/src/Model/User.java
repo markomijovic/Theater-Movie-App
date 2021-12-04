96a0e990feccaee149e016f97d5a7908201b7523
Gabriel Mathias <gabriel.mathias@ucalgary.ca>
2021-12-04 13:08:02 -0700

    Added comments for User

--- Code/TheaterApp/src/Model/User.java
+++ Code/TheaterApp/src/Model/User.java
@@ -1,12 +1,24 @@
 package Model;
 
+/**
+ * This is the base class for a user.
+ */
 public abstract class User {
+	/**
+	 * Holds all information required for payment.
+	 */
 	protected PaymentInfo myPaymentInfo;
 	
+	/**
+	 * Getter for payment information.
+	 */
 	public PaymentInfo getPaymentInfo() {
 		return myPaymentInfo;
 	}
 	
+	/**
+	 * Setter for payment information.
+	 */
 	public void setPaymentInfo(PaymentInfo val) {
 		myPaymentInfo = val;
 	}
