a75b202f4401a658df3c538fcb42c54342db709f
Gabriel Mathias <gabriel.mathias@ucalgary.ca>
2021-12-04 13:05:15 -0700

    Added comments for RegisteredUser

--- Code/TheaterApp/src/Model/RegisteredUser.java
+++ Code/TheaterApp/src/Model/RegisteredUser.java
@@ -1,12 +1,35 @@
 package Model;
 
+/**
+ * This class represents a registered user whose information is stored in a database.
+ */
 public class RegisteredUser extends User{
 	
+	/**
+	 * username for log in. This can be used as a unique id.
+	 */
 	private String username;
+	/**
+	 * password to authenticate log in
+	 */
 	private String password;
+	/**
+	 * First name of user
+	 */
 	private String name;
+	/**
+	 * Address for user
+	 */
 	private String address;
 	
+	/**
+	 * Constructor initializing all data members.
+	 * @param username
+	 * @param password
+	 * @param name
+	 * @param address
+	 * @param myPaymentInfo
+	 */
 	RegisteredUser(String username, String password, String name, String address, PaymentInfo myPaymentInfo){
 		this.username = username;
 		this.password = password;
@@ -15,14 +38,24 @@ public class RegisteredUser extends User{
 		this.myPaymentInfo = myPaymentInfo;
 	}
 	
+	/**
+	 * Checks whether a given password matches the one on file.
+
+	 */
 	public boolean validatePassword(String input) {
 		return password.equals(input);
 	}
 	
+	/**
+	 * Returns the username for the user. This is the unique id for the user.
+	 */
 	public String getUsername() {
 		return username;
 	}
 	
+	/**
+	 * Returns the name of the user.
+	 */
 	public String getName() {
 		return name;
 	}
