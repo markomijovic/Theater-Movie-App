b237e4be32552ba6c8a8bb150cde9a8e76106154
Marko Mijovic <mijovicmarko11@gmail.com>
2021-12-05 19:54:55 -0700

    Added get movies jdbc connector

--- Code/TheaterApp/src/Model/DBLoader.java
+++ Code/TheaterApp/src/Model/DBLoader.java
@@ -1,36 +1,77 @@
 package Model;
 
+import java.lang.reflect.Array;
+import java.sql.*;
 import java.util.ArrayList;
 
 /**
  * This class is used to load data from a database into a model.
  */
 public class DBLoader {
-	
+
+	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/movieapp";
+	static final String USERNAME = "root";
+	static final String PASSWORD = ___;
+
+	public DBLoader() throws SQLException {
+	}
+
 	/** Loads the information for all theaters from storage
-	 * 
+	 *
 	 *  PROMISES: Returns the list of all theaters
 	 *  REQUIRES: Connection to information storage
 	 */
 	public static ArrayList<Theater> loadTheaters(ArrayList<Movie> listOfAllMovies){
-		ArrayList<Theater> myTheaters = new ArrayList<Theater>();
+		ArrayList<Theater> myTheaters = new ArrayList<>();
 		//TODO: Add a couple of theater entries
 		return myTheaters;
 	}
 
 	/** Loads the information for all movies from storage
-	 * 
+	 *
 	 *  PROMISES: Returns the list of all movies
 	 *  REQUIRES: Connection to information storage
 	 */
-	public static ArrayList<Movie> loadMovies(){
+	public static ArrayList<Movie> loadMovies(Theater t) {
 		ArrayList<Movie> myMovies = new ArrayList<Movie>();
-		//TODO: Add a couple of movie entries
+		try {
+			String query = String.format("SELECT movieName, exclusiveNews, showtimeListID FROM movie WHERE theaterName=?;");
+			Connection con=DriverManager.getConnection(
+					DB_URL,USERNAME,PASSWORD);
+			PreparedStatement statement = con.prepareStatement(query);
+			statement.setString(1, t.getTheaterName());
+			ResultSet resultSet = statement.executeQuery();
+			if (resultSet != null) {
+				while (resultSet.next()) {
+					ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+					for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
+						int type = resultSetMetaData.getColumnType(i);
+						if (type == Types.VARCHAR || type == Types.CHAR) {
+							System.out.println(resultSet.getString(i));
+						} else {
+							System.out.println(resultSet.getInt(i));
+						}
+					}
+					System.out.println("-----------");
+				}
+			}
+		} catch(Exception e) {System.out.println(e);}
 		return myMovies;
 	}
 
+	public static ArrayList<Showing> loadShowings(Movie m){
+		ArrayList<Showing> myShowings = new ArrayList<>();
+		return myShowings;
+	}
+
+	public static ArrayList<Ticket> loadTickets(Showing s) {
+		ArrayList<Ticket> myTickets = new ArrayList<>();
+		String query = String.format("");
+		return myTickets;
+	}
 	/** Loads the information for all users from storage
-	 * 
+	 *
 	 *  PROMISES: Returns the list of all users
 	 *  REQUIRES: Connection to information storage
 	 */
@@ -42,7 +83,7 @@ public class DBLoader {
 
 	/** Loads the information for all active vouchers from storage
 	 *  Note that a voucher is only valid for one year after its issue date.
-	 *  
+	 *
 	 *  PROMISES: Returns the list of all active vouchers
 	 *  REQUIRES: Connection to information storage
 	 */
@@ -51,5 +92,18 @@ public class DBLoader {
 		//TODO: Add vouchers
 		// Note that a voucher is only active until the expiry date
 		return myVouchers;
+	}
+
+	public static void main(String[] args) throws SQLException {
+		DBLoader.loadMovies(new Theater("ge", "CrossIron Cineplex"));
+
+		// loadtheater
+		// theater(postalcode: from theater table, dictionary<movie, arraylist<showings>>
+		// -> need movie -> showings
+
+		// loadmovies
+		// movie(title, theaterApp)
+
+		// showing(
 	}
 }
