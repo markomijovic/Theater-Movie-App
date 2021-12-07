2d33a31daa1214ce1eee13ccf046ef506b652240
Gabriel Mathias <gabriel.mathias@ucalgary.ca>
2021-12-06 19:17:14 -0700

    Fixed merge

--- Code/TheaterApp/src/View/Page.java
+++ Code/TheaterApp/src/View/Page.java
@@ -6,13 +6,10 @@ import java.awt.*;
 import java.util.HashMap;
 import javax.swing.*;
 
-<<<<<<< HEAD
+public abstract class Page extends JFrame {
+
 	public abstract String[] interactWithUser();
 	public void sendMessage(String[] message) {}
 	public void loadModelInfo(String[] pageInput) {}
-}
-=======
-public class Page extends JFrame {
 
 }
->>>>>>> 36bad654c4474341043557dba948e56485b3478c
