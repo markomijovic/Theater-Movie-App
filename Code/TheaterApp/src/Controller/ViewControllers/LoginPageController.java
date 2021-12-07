d201cb180d647bcfe959ba879252f7b976932b8c
Sarang K <sarang3@gmail.com>
2021-12-06 21:33:07 -0700

    Login Controller added

---
+++ Code/TheaterApp/src/Controller/ViewControllers/LoginPageController.java
@@ -0,0 +1,44 @@
+package Controller.ViewControllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import View.LoginPage;
+import Model.TheaterApp;
+
+public class LoginPageController extends ViewController {
+	public LoginPage loginView;
+	//public TheaterApp theaterApp;
+	public LoginPageController(LoginPage loginView) {
+		this.loginView = loginView;
+		loginView.addButtonActionListener(new RegUserListener(), new GuestListener());
+	}
+	
+	class RegUserListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		
+			
+		System.out.println("something");
+		String username = loginView.userText.getText();
+		String password = loginView.passwordText.getText();
+		 
+		boolean x = Boolean.parseBoolean(app.login(username, password)[0]);
+		System.out.println(x);
+		
+		}
+	}
+	
+	class GuestListener implements ActionListener {
+		@Override
+		//AppSeting.option="GuestLogin";
+		public void actionPerformed(ActionEvent e) {
+		loginView.setVisible(false);
+		System.out.println("TEST");
+		//showGuestUserPage();
+		}
+	}
+
+}
+
+
